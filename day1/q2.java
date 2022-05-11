36b50ca5bcc80780410bf170d4f68b53e9c68904
shreya40 <88774355+shreya40@users.noreply.github.com>
2022-05-11 14:58:33 +0530

    Create q2.java

---
+++ day1/q2.java
@@ -0,0 +1,36 @@
+[link](https://leetcode.com/problems/set-matrix-zeroes/submissions/)
+```
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       int r=matrix.length;
+        int c=matrix[0].length;
+        int dummy1[]=new int[r];
+        int dummy2[]=new int[c];
+     Arrays.fill(dummy1,-1);
+     Arrays.fill(dummy2,-1);
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==0)
+                    
+                {
+                    dummy1[i]=0;
+                    dummy2[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+              if(dummy1[i]==0||dummy2[j]==0)
+            {
+                matrix[i][j]=0;  
+              }    
+            }
+        }
+    
+}
+}
+```
