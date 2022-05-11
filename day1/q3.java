7fe930e6be6e2e9e2a8b36323c0c67462eedec4b
shreya40 <88774355+shreya40@users.noreply.github.com>
2022-05-11 23:26:10 +0530

    Create q3.java

---
+++ day1/q3.java
@@ -0,0 +1,24 @@
+kadane's algo
+  https://leetcode.com/problems/maximum-subarray/submissions/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0]; 
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+            if(sum>max)
+            {
+                max=sum;
+                
+            }
+            if(sum<0)
+            {
+               sum=0; 
+            }
+        }
+        return max;
+        
+    }
+}
