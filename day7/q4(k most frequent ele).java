8bb271bfc6b25a35ab40345e0b0846e2fbbd17d1
shreya40 <88774355+shreya40@users.noreply.github.com>
2022-05-25 00:22:17 +0530

    Create q4(k most frequent ele).java

---
+++ day7/q4(k most frequent ele).java
@@ -0,0 +1,42 @@
+
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        int cnt=0;
+        for (int i:nums)
+        {
+            if (!hm.containsKey(i))
+            {
+                cnt++;
+                hm.put(i,1);
+            }
+            else
+            {
+                int val=hm.get(i);
+                val++;
+                hm.put(i,val);
+            }
+        }
+        //System.out.println(hm+" "+cnt);
+        int [][]mat=new int [cnt][2];
+        int i=0;
+        for (int g:hm.keySet())
+        {
+            mat[i][0]=g;
+            mat[i][1]=hm.get(g);
+            i++;
+        }
+        Arrays.sort(mat,(a,b)->Integer.compare(a[1],b[1]));
+        int []arr=new int [k];
+        int index=mat.length-1;
+        k--;
+        while(k>=0)
+        {
+            arr[k]=mat[index][0];
+            k--;
+            index--;
+        }
+        return arr;
+    }
+}
